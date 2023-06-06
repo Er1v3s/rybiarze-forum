7e5a1a6031c80ec7a400de734c5b233c10aec449
Filip Statkiewicz <stf6.dev@gmail.com>
2023-06-06 16:11:04 +0200

    Connect project with Database

---
+++ Forum/Data/Post.cs
@@ -0,0 +1,10 @@
+﻿namespace Forum.Data
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Introduction{ get; set; }
+        public string BodyText { get; set; }
+    }
+}
